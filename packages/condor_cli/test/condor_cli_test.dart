1bb6cdece8165b376270d32e650d28d4aed2e56b
LinXunFeng <linxunfeng@yeah.net>
2024-02-22 11:39:30 +0800

    test: flutter

--- packages/condor_cli/test/condor_cli_test.dart
+++ packages/condor_cli/test/condor_cli_test.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'package:condor_cli/src/utils/utils.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  group('Utils - Flutter', () {
+    test('正常获取 Flutter 的版本号', () async {
+      final version = await flutter.frameworkVersion() ?? '';
+      // print('version = $version');
+      expect(version.isNotEmpty, true);
+    });
+  });
+}
